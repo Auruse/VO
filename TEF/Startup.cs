60508482dde411ff0ff60b6d6735591e1c854276
puzin <pam8@yandex.ru>
2018-08-21 10:02:09 +0300

    v 0.1

---
+++ TEF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TEF.Startup))]
+namespace TEF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
